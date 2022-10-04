d9a60de15e1d73f6da84e99b452a10c796ae016f
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-10-04 13:19:11 +0700

    circle done

---
+++ app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
@@ -0,0 +1,80 @@
+package ru.netology.nmedia.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import ru.netology.nmedia.util.AndroidUtils
+import kotlin.math.min
+import kotlin.random.Random
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributesSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : View(
+    context,
+    attributesSet,
+    defStyleAttr,
+    defStyleRes
+) {
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var radius = 0F
+    private var center = PointF()
+    private  var oval = RectF()
+    private val lineWith = AndroidUtils.dp(context, 5)
+    private val paint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = lineWith.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+    private val textPaint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        textSize = AndroidUtils.dp(context,20).toFloat()
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWith
+        center = PointF(w / 2F, h / 2F)
+        oval=RectF(
+            center.x-radius,
+            center.y-radius,
+            center.x+radius,
+            center.y+radius
+        )
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+       if(data.isEmpty()) {
+           return
+       }
+        var startAngle = -90F
+        data.forEach{
+            val angle = it*360
+            paint.color = Random.nextInt(0xFF000000.toInt(),0xFFFFFFFF.toInt())
+            canvas?.drawArc(oval,startAngle,angle,false,paint)
+            startAngle-=angle
+        }
+
+        canvas?.drawText(
+            "%.2f%%".format(data.sum()*100),
+            center.x,
+            center.y+textPaint.textSize/4,
+            textPaint
+        )
+    }
+}
